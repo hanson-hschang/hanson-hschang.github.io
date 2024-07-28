0438c4284b734bc9142e06618b927f42a301b62a
Hanson-HSChang <hanson.hschang@gmail.com>
2024-07-27 19:42:08 -0500

    feat: update markdown template

--- assets/js/blog-post-loader.js
+++ assets/js/blog-post-loader.js
@@ -1,12 +1,34 @@
 // loadPosts.js
 async function fetchContentMd(filePath) {
   try {
+    // Read the content of the file
     const response = await fetch(filePath);
     const text = await response.text();
+
+    // Split the content of the file into entries
     const entries = text.trim().split('\n\n');
     return entries.map(entry => {
-      const [title, date, link, content] = entry.split('\n');
-      return { title, date, link, content};
+
+      // Split the table into lines
+      entry = entry.trim().split('\n')
+
+      // Remove the header and separator lines
+      const content = entry.slice(2);
+
+      // Initialize an object to store the parsed data
+      const parsedData = {};
+
+      // Parse each data line
+      content.forEach(line => {
+        // Split the line by the pipe character and trim each part
+        const [key, value] = line.split('|').map(item => item.trim()).filter(item => item !== '');
+        
+        // Store the key-value pair in the parsedData object
+        if (key && value) {
+          parsedData[key] = value;
+        }
+      });
+      return parsedData;
     });
   } catch (error) {
     console.error('Error fetching content.md:', error);
@@ -31,7 +53,7 @@ function createPostEntry(post, template) {
     .replace('${link}', post.link)
     .replace('${title}', post.title)
     .replace('${date}', post.date)
-    .replace('${content}', post.content);
+    .replace('${excerpt}', post.excerpt);
   const element = document.createElement('div');
   element.innerHTML = filledTemplate.trim();
   return element.firstChild;
